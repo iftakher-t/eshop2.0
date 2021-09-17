0f58eff81872c8c96cb21b89b087958e782134cd
Md Iftakher Hossain <hmdiftakher@gmail.com>
2021-09-17 20:20:10 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import Navbar from './Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
+import {BrowserRouter} from 'react-router-dom';
 
 
 function App() {
   return (
 
     <div className="App">
+      <BrowserRouter>
         <Navbar />
+        </BrowserRouter>
     </div>
   );
 } 
---
+++ src/components/Navbar/Menu.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {BrowserRouter, NavLink } from 'react-router-dom';
+
+const Menu = () => {
+    return (
+        <>
+
+        <NavLink>All Shop</NavLink>
+        <NavLink>Catagories</NavLink>
+        <NavLink>Top-up</NavLink>
+        <NavLink>Express</NavLink>
+        <NavLink>T 10</NavLink>
+
+        <NavLink>News Feed</NavLink>
+        <NavLink>Merchant Zone</NavLink>
+        <NavLink>Help</NavLink>
+        </>
+    )
+}
+
+export default Menu
